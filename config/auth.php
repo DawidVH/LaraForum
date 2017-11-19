6a9a5c706328b1bcd13f025db9549683f6d4bd12
dawidvh <hochmandawid@gmail.com>
2017-11-19 14:07:16 +0000

    adding roles

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,14 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\User::class,
+            'table' => 'users',
         ],
+
+//    'providers' => [
+//        'users' => [
+//            'driver' => 'eloquent',
+//            'model' => App\User::class,
+//        ],
 
         // 'users' => [
         //     'driver' => 'database',
